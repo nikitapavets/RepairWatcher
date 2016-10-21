7fc043135b906a99beabee296897e2bccfdecdc5
nikitapavets <nikitapavets@gmail.com>
2016-10-21 23:58:17 +0300

    Add main logo in /progress page

--- src/resources/views/progress/progress.blade.php
+++ src/resources/views/progress/progress.blade.php
@@ -10,46 +10,78 @@
 
                 <div class="container">
                     <div class="row">
+                        <div class="col-md-12">
 
-                        <div class="location">
-                            <svg class="contacts__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                <use xlink:href='#location'></use>
-                            </svg>
-                            <a href="https://www.google.ru/maps/place/54%C2%B053'19.7%22N+28%C2%B041'33.1%22E/@54.888794,28.6906765,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d54.888794!4d28.692517" target="_blank">
-                                РБ, г. Лепель, ул. Ленинская, д. 9, офис 1
-                            </a>
+                            <div class="location">
+                                <svg class="contacts__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                    <use xlink:href='#location'></use>
+                                </svg>
+                                <a href="https://www.google.ru/maps/place/54%C2%B053'19.7%22N+28%C2%B041'33.1%22E/@54.888794,28.6906765,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d54.888794!4d28.692517" target="_blank">
+                                    РБ, г. Лепель, ул. Ленинская, д. 9, офис 1
+                                </a>
+                            </div>
+
+                            <div class="phones">
+
+                                <div class="phone">
+                                    <svg class="contacts__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                        <use xlink:href='#phone-call'></use>
+                                    </svg>
+                                    +375(29)717-58-04
+                                </div>
+
+                                <div class="phone">
+                                    <svg class="contacts__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                        <use xlink:href='#phone-call'></use>
+                                    </svg>
+                                    +375(29)948-41-94
+                                </div>
+
+                                <div class="phone">
+                                    <svg class="contacts__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                        <use xlink:href='#circular-clock'></use>
+                                    </svg>
+                                    Пн-Пт 9:00-18:00, Сб-Вс 9:00-14-00
+                                </div>
+
+                            </div>
+
                         </div>
-
-                        <div class="phones">
-
-                            <div class="phone">
-                                <svg class="contacts__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <use xlink:href='#phone-call'></use>
-                                </svg>
-                                +375(29)717-58-04
-                            </div>
-
-                            <div class="phone">
-                                <svg class="contacts__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <use xlink:href='#phone-call'></use>
-                                </svg>
-                                +375(29)948-41-94
-                            </div>
-
-                            <div class="phone">
-                                <svg class="contacts__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <use xlink:href='#circular-clock'></use>
-                                </svg>
-                                Пн-Пт 9:00-18:00, Сб-Вс 9:00-14-00
-                            </div>
-
-                        </div>
-
                     </div>
                 </div>
 
             </div>
+
+            <div class="main-navigation">
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+
+                            <div class="main-logo">
+
+                                <a href="#">
+                                    <svg class="main-logo__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                        <use xlink:href='#wrench_tool'></use>
+                                    </svg>
+                                    <div class="main-logo__title">
+                                        <span class="main-logo__title--first">Repair</span> <span class="main-logo__title--second">Watcher</span>
+                                    </div>
+                                </a>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
         </header>
+
+        <div class="slider">
+
+        </div>
 
     </section>
 
