5f5b55d04ed1a7702a435998a830384ea0f9e658
nikitapavets <nikitapavets@gmail.com>
2016-10-26 01:30:53 +0300

    Add admin panel

--- src/resources/views/admin/repair/list.blade.php
+++ src/resources/views/admin/repair/list.blade.php
@@ -27,3 +27,118 @@
         @include('admin.repair._repairs_table')
     </div>
 </div>
+
+/*@section('admin_content')
+
+    <header class="content--title">
+        <p>Добавить в ремонт</p>
+    </header>
+
+    <div class="card">
+
+        {{ Form::open(array('url' => 'repair')) }}
+
+        <div class="row">
+
+            <div class="col-md-4 col-xs-12">
+
+                <div class="info--person">
+
+                    <header class="card--header">
+                        <p>Клиент</p>
+                    </header>
+
+                    <div class="input-field">
+                        <input id="person_second_name" type="text" class="validate" name="person_second_name" pattern="^[а-яА-Я]+$">
+                        <label for="person_second_name">Фамилия</label>
+                    </div>
+                    <div class="input-field">
+                        <input id="person_first_name" type="text" class="validate" name="person_first_name" pattern="^[а-яА-Я]+$">
+                        <label for="person_first_name">Имя</label>
+                    </div>
+                    <div class="input-field">
+                        <input id="person_father_name" type="text" class="validate" name="person_father_name" pattern="^[а-яА-Я]+$">
+                        <label for="person_father_name">Отчество</label>
+                    </div>
+                    <div class="input-field">
+                        <input id="person_address" type="text" class="validate" name="person_address">
+                        <label for="person_address">Адрес</label>
+                    </div>
+                    <div class="input-field">
+                        <input id="person_tel" type="tel" class="validate" name="person_tel" pattern="^375(29|33|44)[0-9]{7}$">
+                        <label for="person_tel">Номер телефона</label>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="col-md-4 col-xs-12">
+
+                <div class="info--product">
+
+                    <header class="card--header">
+                        <p>Продукт</p>
+                    </header>
+
+                    <div class="input-field">
+                        <input id="product_title" type="text" class="validate" name="product_title">
+                        <label for="product_title">Наименование</label>
+                    </div>
+                    <div class="input-field">
+                        <input id="product_code" type="text" class="validate" name="product_code">
+                        <label for="product_code">Зав. номер</label>
+                    </div>
+                    <div class="input-field">
+                        <input id="product_set" type="text" class="validate" name="product_set">
+                        <label for="product_set">Комплектность</label>
+                    </div>
+                    <div class="input-field">
+                        <textarea id="product_diagnosis" class="materialize-textarea" name="product_diagnosis"></textarea>
+                        <label for="product_diagnosis">Неисправность</label>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="col-md-4 col-xs-12">
+
+                <div class="info--btn">
+
+                    <header class="card--header">
+                        <p>Информация</p>
+                    </header>
+
+                    <div class="input-field">
+                        <input id="info_receipt_number" type="text" class="validate" name="info_receipt_number" pattern="[0-9]+">
+                        <label for="info_receipt_number">Номер квитанции</label>
+                    </div>
+                    <div class="input-field">
+                        <input type="date" class="datepicker" name="info_date" id="info_date" data-value="today">
+                        <label for="info_date">Дата</label>
+                    </div>
+
+                    <p>
+                        <input name="info_maintenance_type" class="with-gap" type="radio" value="paid" id="info_maintenance_type1" checked/>
+                        <label for="info_maintenance_type1">Платный ремонт</label>
+                    </p>
+                    <p>
+                        <input name="info_maintenance_type" class="with-gap" type="radio" value="free" id="info_maintenance_type2"/>
+                        <label for="info_maintenance_type2">Гарантийный ремонт</label>
+                    </p>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="button_wrap">
+                <button class="btn waves-effect waves-light" type="submit">Добавить</button>
+            </div>
+        </div>
+
+        {{ Form::close() }}
+
+    </div>
+
+@stop
--- src/resources/views/layouts/admin_layout.blade.php
+++ src/resources/views/layouts/admin_layout.blade.php
@@ -2,27 +2,105 @@
 
 @section('content')
 
-    <div class="admin_layout">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
+    <div class="wrapper">
+        <div class="content admin_layout">
 
-                    <header class="main-header">
+            <header class="main-header">
 
-                        <div class="logo">
-                            <div class="logo--svg">
-
-                            </div>
-                            <div class="logo--label"><span class="any">Any</span><span class="comp">Comp</span></div>
-                        </div>
-
-                    </header>
-
+                <div class="logo">
+                    <div class="logo--svg">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <use xlink:href='#teamwork'></use>
+                        </svg>
+                    </div>
+                    <div class="logo--label"><span class="any">Any</span><span class="comp">Comp</span></div>
                 </div>
-            </div>
+
+                <nav class="top-menu">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <svg class="top-menu__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                    <use xlink:href='#settings_solid'></use>
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <svg class="top-menu__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                    <use xlink:href='#search_solid'></use>
+                                </svg>
+                            </a>
+                        </li>
+                        <li>
+                            <a class="profile" href="#">
+                                <img src="/img/team/nikita.png" alt="">
+                                <span>Никита Павец</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+            </header>
+
+            <section class="dashboard_wrap">
+                <nav class="left-menu">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <div class="item-ico">
+                                    <svg class="top-menu__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                        <use xlink:href='#plus_solid'></use>
+                                    </svg>
+                                </div>
+                                <div class="item-text">
+                                    Добавить в ремонт
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <div class="item-ico">
+                                    <svg class="top-menu__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                        <use xlink:href='#list_solid'></use>
+                                    </svg>
+                                </div>
+                                <div class="item-text">
+                                    Техника в ремонте
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <div class="item-ico">
+                                    <svg class="top-menu__svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                        <use xlink:href='#users_solid'></use>
+                                    </svg>
+                                </div>
+                                <div class="item-text">
+                                    Клиенты
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div class="admin_content--wrap">
+                    <div class="admin_content">
+                        <header class="admin_content--header">
+                            Добавление товара
+                        </header>
+
+                        <div class="admin_content--content">
+                            @yield('admin_content')
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
+
         </div>
     </div>
-
-    @yield('admin_content')
 
 @stop
